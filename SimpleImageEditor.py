7110a610fa3a94423918712a8796d9d1acbb17f9
Lukáš Litvan <lukas.litvan@gmail.com>
2018-12-12 00:32:22 +0100

    Add method to generate images, each with different operation.

--- SimpleImageEditor.py
+++ SimpleImageEditor.py
@@ -49,3 +49,44 @@ class SimpleImageEditor:
 
     def enhance_edges(self):
         ...
+
+    def save_each(self):
+        # rotate cw
+        self.reset_image()
+        self.rotate_cw()
+        self.save_image("rotated_cw.jpg")
+
+        # rotate ccw
+        self.reset_image()
+        self.rotate_ccw()
+        self.save_image("rotated_ccw.jpg")
+
+        # mirror
+        self.reset_image()
+        self.mirror()
+        self.save_image("mirrored.jpg")
+
+        # invert colors
+        self.reset_image()
+        self.invert_colors()
+        self.save_image("inverted_colors.jpg")
+
+        # greyscale
+        self.reset_image()
+        self.greyscale()
+        self.save_image("greyscaled.jpg")
+
+        # lighten
+        self.reset_image()
+        self.lighten()
+        self.save_image("lightened.jpg")
+
+        # darken
+        self.reset_image()
+        self.darken()
+        self.save_image("darkened.jpg")
+
+        # enhance edges
+        self.reset_image()
+        self.enhance_edges()
+        self.save_image("enhanced_edges.jpg")
