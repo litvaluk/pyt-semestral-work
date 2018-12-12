6b974e6954c45195b3f7f4d0101ac268eacb525f
Lukáš Litvan <lukas.litvan@gmail.com>
2018-12-12 12:18:55 +0100

    Implement all remaining image operations.

--- SimpleImageEditor.py
+++ SimpleImageEditor.py
@@ -22,33 +22,60 @@ class SimpleImageEditor:
         self.actual = self.original.copy()
 
     def rotate_cw(self):
-        ...
+        self.actual = np.rot90(self.actual, -1)
 
     def rotate_ccw(self):
-        ...
+        self.actual = np.rot90(self.actual)
 
-    def mirror(self):
-        ...
+    def mirror_x(self):
+        self.actual = self.actual[::-1]
+
+    def mirror_y(self):
+        self.actual = self.actual[::, ::-1]
 
     def invert_colors(self):
-        ...
+        self.actual = 255 - self.actual
 
     def greyscale(self):
         for y in range(self.actual.shape[1]):
             for x in range(self.actual.shape[0]):
-                gray_component = 0.299 * self.actual[x][y][0] + 0.587 * self.actual[x][y][1] + 0.114 * self.actual[x][y][2]
-                self.actual[x, y, 0] = gray_component
-                self.actual[x, y, 1] = gray_component
-                self.actual[x, y, 2] = gray_component
+                grey_component = 0.299 * self.actual[x][y][0] + 0.587 * self.actual[x][y][1] + 0.114 * \
+                                 self.actual[x][y][2]
+                self.actual[x, y, 0] = grey_component
+                self.actual[x, y, 1] = grey_component
+                self.actual[x, y, 2] = grey_component
 
     def lighten(self):
-        ...
+        self.actual = self.actual + (256 - self.actual) * 0.2
 
     def darken(self):
-        ...
+        self.actual = self.actual * 0.75
 
     def enhance_edges(self):
-        ...
+        new = self.actual.copy()
+        enhance_filter = np.asarray([[ 0, -1,  0],
+                                     [-1,  5, -1],
+                                     [ 0, -1,  0]])
+
+        for x in range(1, new.shape[0] - 1):
+            for y in range(1, new.shape[1] - 1):
+                for c in range(new.shape[2]):
+                    component = (enhance_filter[0][0] * self.actual[x - 1, y - 1, c]
+                                 + enhance_filter[0][1] * self.actual[x - 1, y, c]
+                                 + enhance_filter[0][2] * self.actual[x - 1, y + 1, c]
+                                 + enhance_filter[1][0] * self.actual[x, y - 1, c]
+                                 + enhance_filter[1][1] * self.actual[x, y, c]
+                                 + enhance_filter[1][2] * self.actual[x, y + 1, c]
+                                 + enhance_filter[2][0] * self.actual[x + 1, y - 1, c]
+                                 + enhance_filter[2][1] * self.actual[x + 1, y, c]
+                                 + enhance_filter[2][2] * self.actual[x + 1, y + 1, c])
+                    if component > 255:
+                        component = 255
+                    if component < 0:
+                        component = 0
+                    new[x, y, c] = component
+
+        self.actual = new
 
     def save_each(self):
         # rotate cw
@@ -61,10 +88,15 @@ class SimpleImageEditor:
         self.rotate_ccw()
         self.save_image("rotated_ccw.jpg")
 
-        # mirror
+        # mirror x
         self.reset_image()
-        self.mirror()
-        self.save_image("mirrored.jpg")
+        self.mirror_x()
+        self.save_image("mirrored_x.jpg")
+
+        # mirror y
+        self.reset_image()
+        self.mirror_y()
+        self.save_image("mirrored_y.jpg")
 
         # invert colors
         self.reset_image()
